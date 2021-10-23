1b5a4237dfc03271f543645e8a768fbe99d0b88f
Nathan Kipp <nskipp@gmail.com>
2021-10-23 17:52:12 -0500

    bugfix show-picture

--- docs/index.js
+++ docs/index.js
@@ -1,17 +1,20 @@
 function setModalContentDisplay(value) {
   $('.modal-content').css({ display: value });
 }
+const hideModalContentDisplay = () => setModalContentDisplay('none');
+const unsetModalContentDisplay = () => setTimeout(() => setModalContentDisplay('unset'), 500);
 
 // close modals when click is outside .modal-content
 $('.reveal .bg-section').on('click', (e) => {
   try {
     $(e.target).parent('div').foundation('close');
-    setTimeout(() => setModalContentDisplay('unset'), 500);
+    unsetModalContentDisplay();
   } catch (e) {
     void 0;
   }
 });
 // but don't close modals when clicks are IN the .modal-content
 $('.modal-content').on('click', (e) => e.stopPropagation());
-// show picture
-$('.show-picture').on('click', () => setModalContentDisplay('none'));
+// show picture & reset when modal closed
+$('.show-picture').on('click', hideModalContentDisplay);
+$('.close-button').on('click', unsetModalContentDisplay);
