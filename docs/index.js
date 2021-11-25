52b0737baaa242db823dc80089a4c2e1806fd558
Nathan Kipp <nathan.kipp@servicenow.com>
2021-11-25 17:50:15 -0500

    sliding menu

--- docs/index.js
+++ docs/index.js
@@ -1,20 +1,27 @@
 function setModalContentDisplay(value) {
-  $('.modal-content').css({ display: value });
+  $(".modal-content").css({ display: value });
 }
-const hideModalContentDisplay = () => setModalContentDisplay('none');
-const unsetModalContentDisplay = () => setTimeout(() => setModalContentDisplay('unset'), 500);
+const hideModalContentDisplay = () => setModalContentDisplay("none");
+const unsetModalContentDisplay = () =>
+  setTimeout(() => setModalContentDisplay("unset"), 500);
 
 // close modals when click is outside .modal-content
-$('.reveal .bg-section').on('click', (e) => {
+$(".reveal .bg-section").on("click", (e) => {
   try {
-    $(e.target).parent('div').foundation('close');
-    unsetModalContentDisplay();
+    $(e.target).parent("div").foundation("close");
   } catch (e) {
     void 0;
   }
 });
+$("body").on("closed.zf.reveal", unsetModalContentDisplay);
 // but don't close modals when clicks are IN the .modal-content
-$('.modal-content').on('click', (e) => e.stopPropagation());
+$(".modal-content").on("click", (e) => e.stopPropagation());
 // show picture & reset when modal closed
-$('.show-picture').on('click', hideModalContentDisplay);
-$('.close-button').on('click', unsetModalContentDisplay);
+$(".show-picture").on("click", hideModalContentDisplay);
+$(".close-button").on("click", unsetModalContentDisplay);
+
+// info-icons
+$(".info-icon").on("click", () => {
+  $(".info-icons").toggleClass("show");
+  $(".info-icon").toggleClass("show");
+});
