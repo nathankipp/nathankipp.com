e3782175701f702d7ea0c879464ff55f505ae000
Nathan Kipp <nskipp@gmail.com>
2021-11-28 21:35:12 -0600

    minify

--- gulpfile.js
+++ gulpfile.js
@@ -2,39 +2,40 @@
 
 const browserSync = require("browser-sync").create();
 const { watch, src, dest } = require("gulp");
+const minify = require("gulp-minify");
 const sass = require("gulp-sass")(require("sass"));
 const concat = require("gulp-concat");
 
 const BUILD_DIR = "./docs";
 
+const minime = () => src(['./index.js'])
+    .pipe(minify({ noSource: true }))
+    .pipe(dest(`${BUILD_DIR}/`));
+
 const sassify = () => src('./*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(dest(`${BUILD_DIR}/`));
-
-const COPY = [
-  { from: './index.js', to: `${BUILD_DIR}/` }
-];
-const copyFromTo = ({ from, to }) => src(from).pipe(dest(to));
-const copy = () => COPY.forEach(copyFromTo);
 
 const writeIndexHtml = () =>
   src(["./partials/header.html", "./body.html", "./partials/footer.html"])
     .pipe(concat("index.html"))
     .pipe(dest(`${BUILD_DIR}/`));
 
-function defaultTask(cb) {
+const build = () => {
+  minime();
   sassify();
-  copy();
   writeIndexHtml();
+}
+
+function defaultTask(cb) {
+  build();
 
   browserSync.init({
     server: { baseDir: BUILD_DIR },
   });
 
   watch(["./index.*", "./body.html", "./partials/*.*", "./scss/*.*"], function (cb) {
-    sassify();
-    copy();
-    writeIndexHtml();
+    build();
     browserSync.reload();
     cb();
   });
