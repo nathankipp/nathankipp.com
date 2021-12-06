c22a4582831983f5dbe8f83ae9e54bdc9cc49924
Nathan Kipp <nskipp@gmail.com>
2021-12-05 22:44:47 -0600

    expand/contract bg images

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-function setModalContentDisplay(value) {
+const ICON = {
+  COMPRESS: 'fa-compress-arrows-alt',
+  EXPAND: 'fa-expand-arrows-alt',
+};
+
+const pictureControls = (toggle) =>
+  $('.compress-expand').toggleClass('hide', toggle);
+
+const showWholePicture = (toggle) => {
+  $('.bg-section').toggleClass('contain', toggle);
+  if (toggle) {
+    $(`.icon.${ICON.COMPRESS}`)
+      .addClass(ICON.EXPAND)
+      .removeClass(ICON.COMPRESS);
+  } else {
+    $(`.icon.${ICON.EXPAND}`).addClass(ICON.COMPRESS).removeClass(ICON.EXPAND);
+  }
+};
+
+const setModalContentDisplay = (value) =>
   $('.modal-content').css({ display: value });
-}
-const hideModalContentDisplay = () => setModalContentDisplay('none');
-const unsetModalContentDisplay = () =>
+const hideModalContentDisplay = () => {
+  setModalContentDisplay('none');
+  pictureControls(false);
+};
+const unsetModalContentDisplay = () => {
   setTimeout(() => setModalContentDisplay('unset'), 500);
+  pictureControls(true);
+};
 
 // close modals when click is outside .modal-content
 $('.reveal .bg-section').on('click', (e) => {
   try {
+    showWholePicture(false);
     $(e.target).parent('div').foundation('close');
   } catch (e) {
     void 0;
   }
 });
+$('.close-button').on('click', unsetModalContentDisplay);
 $('body').on('closed.zf.reveal', unsetModalContentDisplay);
+
 // but don't close modals when clicks are IN the .modal-content
 $('.modal-content').on('click', (e) => e.stopPropagation());
+
 // show picture & reset when modal closed
 $('.show-picture').on('click', hideModalContentDisplay);
-$('.close-button').on('click', unsetModalContentDisplay);
+
+// allow background images to be shown without cropping
+$('.compress-expand').on('click', function (e) {
+  e.stopPropagation();
+  const isCover = $(this).children('.icon').hasClass(ICON.COMPRESS);
+  showWholePicture(isCover);
+  $('.bg-section').toggleClass('contain', isCover);
+});
 
 // info-icons
 $('.info-icon').on('click', () => {
